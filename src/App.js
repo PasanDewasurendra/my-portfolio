e8e6d7fcd3a64fd6382f98a756610299bae2e320
PasanDewasurendra <pasandewasurendra@gmail.com>
2020-07-08 12:58:18 +0530

    initialize my portfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import profilePicture from './images/pkd.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Portfolio</h1>
+      <h2>Pasan Dewasurendra</h2>
+      <img src={profilePicture} alt="avatar" />
     </div>
   );
 }
